10b990f54861e788f094d77123fca6a376f81bee
Zhemchuzhnyi Andrii <n3kk3ll@gmail.com>
2022-06-04 14:00:50 +0300

    add form sending and sending files on server script

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// const dist = "../../../../OpenServer/domains/Picture";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
---
+++ src/js/modules/forms.js
@@ -0,0 +1,104 @@
+const forms = () => {
+  const allForms = document.querySelectorAll(`form`),
+    inputs = document.querySelectorAll(`input`),
+    uploadInputs = document.querySelectorAll(`[name=upload]`);
+
+  const message = {
+    processing: `Идёт отправка`,
+    success: `Отправлено`,
+    failure: `Ошибка`,
+    loading: `assets/img/spinner.gif`,
+    ok: `assets/img/ok.png`,
+    wrong: `assets/img/fail.png`
+  };
+
+  const postData = async (url, data) => {
+    let res = await fetch(url, {
+      method: `POST`,
+      body: data,
+    });
+    return await res.text();
+  };
+
+  const pathes = {
+    sendFileTo: `assets/server.php`,
+    sendQuestionTo: `assets/questions.php`
+  };
+
+  const clearInputs = () => {
+    inputs.forEach(input => input.value = ``);
+    uploadInputs.forEach(upload => {
+      upload.previousElementSibling.textContent = `Файл не выбран`;
+    });
+    if(document.querySelector(`textarea`)) {
+      document.querySelector(`textarea`).value = ``;
+    }
+  };
+
+  allForms.forEach(form => {
+    form.addEventListener(`submit`, e => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement(`div`);
+      statusMessage.classList.add(`text-center`);
+      form.parentNode.appendChild(statusMessage);
+      form.classList.add(`animated`, `fadeOutUp`);
+      setTimeout(() => {
+        form.style.display = `none`;
+      }, 400);
+
+      let statusImage = document.createElement(`img`);
+      statusImage.setAttribute(`src`, message.loading);
+      statusImage.classList.add(`animated`, `fadeInUp`);
+      statusMessage.appendChild(statusImage);
+
+      let statusText = document.createElement(`div`);
+      statusText.textContent = message.processing;
+      statusText.classList.add(`animated`, `fadeIn`);
+      statusText.style.animationDelay = `1s`;
+      statusMessage.appendChild(statusText);
+
+      const formData = new FormData(form);
+      let api;
+
+      form.closest(`.popup-design`) || form.getAttribute(`data-calc`) === `true` ? api = pathes.sendFileTo : api = pathes.sendQuestionTo;
+
+      if(form.getAttribute(`data-calc`) === `end`) {
+        for(let key in userData) {
+          formData.append(key, userData[key]);
+        }
+      }
+
+      postData(api, formData)
+      .then((res) => {
+        console.log(res);
+        statusImage.setAttribute(`src`, message.ok);
+        statusText.textContent = message.success;
+      })
+      .catch(() => {
+        statusImage.setAttribute(`src`, message.wrong);
+        statusText.textContent = message.failure;
+      })
+      .finally(() => {
+        clearInputs();
+        setTimeout(() => {
+          form.style.display = `block`;
+          form.classList.remove(`fadeOutUp`);
+          form.classList.add(`fadeInDown`);
+          statusMessage.remove();
+        }, 5000);
+      });
+    });
+  });
+
+  uploadInputs.forEach(upload => {
+    upload.addEventListener(`input`, () => {
+      const uploadedFileName = upload.files[0].name.split('.'),
+            uploadedFileNameDisplayed = uploadedFileName[0].length > 6 ? `${uploadedFileName[0].substring(0, 6)}...${uploadedFileName[1]}` : `${uploadedFileName[0]}.${uploadedFileName[1]}`;
+      upload.previousElementSibling.textContent = uploadedFileNameDisplayed;
+          });
+  });
+
+};
+
+export default forms;
